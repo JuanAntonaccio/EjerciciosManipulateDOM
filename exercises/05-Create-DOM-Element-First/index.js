457384d998f19629662eb4c4d7d28c5f6d98bde9
JuanAntonaccio <jmantonaccio@gmail.com>
2021-09-02 00:44:56 +0000

    hasta ejercicio 9

--- exercises/05-Create-DOM-Element-First/index.js
+++ exercises/05-Create-DOM-Element-First/index.js
@@ -4,3 +4,8 @@ document.querySelector("#myDiv").appendChild(elem);
 
 //Your code here
 // Don'change the code above
+
+let seg = document.createElement("p");
+seg.innerHTML = "Hello World";
+seg.style.background="yellow";
+document.querySelector("#myDiv").appendChild(seg);
